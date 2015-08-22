ae5899234d42b14c63e8517b27bb8c4ac5b78028
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-08-22 19:15:59 +0100

    (4) Module 4 - CORS and Formatters complete

--- APM/APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM/APM.WebAPI/App_Start/WebApiConfig.cs
@@ -8,23 +8,28 @@ using Newtonsoft.Json.Serialization;
 
 namespace APM.WebAPI
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            //config.SuppressDefaultHostAuthentication();
-            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			// Configure Web API to use only bearer token authentication.
+			//config.SuppressDefaultHostAuthentication();
+			//config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.EnableCors();
+
+			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+				new CamelCasePropertyNamesContractResolver();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
