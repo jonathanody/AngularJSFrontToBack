3a19e212a607e881aba4ca35d39630d29d75aed4
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-08-22 19:38:36 +0100

    (5) Passing parameters by extending the URL path

--- APM.Client/app/common/productResource.js
+++ APM.Client/app/common/productResource.js
@@ -7,6 +7,6 @@
 			["$resource", "appSettings", productResource]);
 
 	function productResource($resource, appSettings) {
-		return $resource(appSettings.serverPath + "/api/products/:id");
+		return $resource(appSettings.serverPath + "/api/products/:search");
 	}
 }());
