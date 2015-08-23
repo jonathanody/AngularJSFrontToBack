da7d5dbd90cb1f70229ea8053f79b78d8254ea29
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-08-23 10:42:09 +0100

    (8) Model 8 - Action Results, Error Handling & Validation complete

--- APM/APM.WebAPI/Models/Product.cs
+++ APM/APM.WebAPI/Models/Product.cs
@@ -19,8 +19,8 @@ namespace APM.WebAPI.Models
 		public int ProductId { get; set; }
 
 		[Required(ErrorMessage = "Product Name is required", AllowEmptyStrings = false)]
-		[MinLength(5, ErrorMessage = "Product Name min length is 5 characters")]
-		[MaxLength(11, ErrorMessage = "Product Name max length is 11 characters")]
+		[MinLength(4, ErrorMessage = "Product Name min length is 4 characters")]
+		[MaxLength(12, ErrorMessage = "Product Name max length is 12 characters")]
 		public string ProductName { get; set; }
 		
 		public DateTime ReleaseDate { get; set; }
