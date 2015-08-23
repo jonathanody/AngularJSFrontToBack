307638aaa4a07b4a2ba9f5b2c2392a08a122678b
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-08-23 19:42:18 +0100

    (10) Added registering a user

---
+++ APM.Client/app/common/userAccount.js
@@ -0,0 +1,16 @@
+﻿(function () {
+	"use strict";
+	
+	angular
+		.module("common.services")
+		.factory("userAccount",
+			["$resource", "appSettings", userAccount]);
+
+	function userAccount($resource, appSettings) {
+		return $resource(appSettings.serverPath + "/api/Account/Register", null,
+			{
+				'registerUser': { method: 'POST' }
+			});
+	}
+
+}());
