f12b033ba507d3e79c4f51249dd1c9421ef292e5
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-08-30 16:53:00 +0100

    (11) Module 11 - User Authorization complete

--- APM.Client/app/mainCtrl.js
+++ APM.Client/app/mainCtrl.js
@@ -4,11 +4,13 @@
 	angular
 		.module("productManagement")
 		.controller("MainCtrl",
-			["userAccount", MainCtrl]);
+			["userAccount", "currentUser", MainCtrl]);
 
-	function MainCtrl(userAccount) {
+	function MainCtrl(userAccount, currentUser) {
 		var vm = this;
-		vm.isLoggedIn = false;
+		vm.isLoggedIn = function () {
+			return currentUser.getProfile().isLoggedIn;
+		};
 		vm.message = '';
 		vm.userData = {
 			userName: '',
@@ -46,14 +48,12 @@
 
 			userAccount.login.loginUser(vm.userData,
 				function (data) {
-					vm.isLoggedIn = true;
 					vm.message = "";
 					vm.password = "";
-					vm.token = data.access_token;
+					currentUser.setProfile(vm.userData.userName, data.access_token);
 				},
 				function (response) {
 					vm.password = "";
-					vm.isLoggedIn = false;
 					vm.message = response.statusText + "\r\n";
 					if (response.data.exceptionMessage) {
 						vm.message += response.data.exceptionMessage;
--- APM/APM.WebAPI/Controllers/ProductsController.cs
+++ APM/APM.WebAPI/Controllers/ProductsController.cs
@@ -32,6 +32,7 @@ namespace APM.WebAPI.Controllers
 
 		// GET: api/Products/5
 		[ResponseType(typeof(Product))]
+		[Authorize()]
 		public IHttpActionResult Get(int id)
 		{
 			try 
